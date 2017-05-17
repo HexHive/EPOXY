42e302c41b74aac72a800a501e79cd505767e00b
Abe Clements <clemen19@purdue.edu>
2017-05-17 10:56:33 -0400

    Uncommented clone steps

--- get_repos.sh
+++ get_repos.sh
@@ -1,11 +1,6 @@
 
 cd ..
-#git@github.com:HexHive/EPOXY-clang.git
-#git@github.com:HexHive/EPOXY-llvm.git
+git clone git@github.com:HexHive/EPOXY-clang.git
+git clone git@github.com:HexHive/EPOXY-llvm.git
 
 ln -s `pwd`/EPOXY-clang EPOXY-llvm/tools/clang
-
-
-
-
-
